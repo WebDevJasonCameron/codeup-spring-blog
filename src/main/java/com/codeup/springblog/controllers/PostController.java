0dae20c8c2aae744391856a96510022ac750f9ec
Jason Cameron <dev.jasoncameron@gmail.com>
2022-05-16 11:28:49 -0500

    comp views ex

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -20,30 +20,35 @@ public class PostController {
 
     @GetMapping("/{userChoice}")
     public String showSinglePost(@PathVariable String userChoice, Model model){
+        Post post = new Post();
+        List<Post> posts = new ArrayList<>();
+
         if(userChoice.equals("single")){
-            Post post = post1;
-            model.addAttribute("post", post);
+            post = post1;
         } else if (userChoice.equals("all")){
-            List<Post> posts = allPosts;
-            model.addAttribute("posts", posts);
+            posts = allPosts;
         }
+
+        model.addAttribute("userChoice", userChoice);
+        model.addAttribute("post", post);
+        model.addAttribute("posts", posts);
         return "posts/show";
     }
 
-    @GetMapping("{id}")
-    public String postById(@PathVariable long id){
-        return "here is the post " + id ;
-    }
-
-    @GetMapping("/create")
-    public String createPost(){
-        return "you just created a post";
-    }
-
-    @PostMapping("/create")
-    public String submitPost(){
-        return "you just created a post";
-    }
+//    @GetMapping("{id}")
+//    public String postById(@PathVariable long id){
+//        return "here is the post " + id ;
+//    }
+//
+//    @GetMapping("/create")
+//    public String createPost(){
+//        return "you just created a post";
+//    }
+//
+//    @PostMapping("/create")
+//    public String submitPost(){
+//        return "you just created a post";
+//    }
 
     Post post1 = new Post("2022/05/15, It takes Three to Pair Program", "Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet, consectetur, adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?\n" +
             "\n");
